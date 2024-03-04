12999c6bcc13f0d4962a0e527349bbea83f2bc7d
Alexpm27 <213523@ids.upchiapas.edu.mx>
2024-03-04 11:12:07 -0600

    UPDATE productions

--- src/scripts/pila.js
+++ src/scripts/pila.js
@@ -2,57 +2,57 @@ function validateVariableDeclaration(value) {
   const tokens = value.match(/\b\w+\b|\S/g);
   let pila = ["$", "S"];
   let apuntador = 0;
-  let infoPila = [];
+  let stackInfo = [];
 
   while (pila.length > 0) {
     const X = pila.at(-1);
     const a = tokens[apuntador];
     if (X === "$") {
-      manejarPila("Pop", X, tokens, apuntador, pila, infoPila);
+      handleStack("Pop", X, tokens, apuntador, pila, stackInfo);
       break;
     }
 
     if (X === a) {
-      manejarPila("Pop", X, tokens, apuntador, pila, infoPila);
+      handleStack("Pop", X, tokens, apuntador, pila, stackInfo);
       apuntador++;
-    } else if (esNoTerminal(X)) {
-      const produccion = obtenerProduccion(X, a);
+    } else if (isNotTerminal(X)) {
+      const produccion = getProduction(X, a);
       if (produccion) {
-        manejarPila("Pop", X, tokens, apuntador, pila, infoPila);
-        manejarPila("Push", X, tokens, apuntador, pila, infoPila);
+        handleStack("Pop", X, tokens, apuntador, pila, stackInfo);
+        handleStack("Push", X, tokens, apuntador, pila, stackInfo);
         if (produccion[0] !== "ε") {
           for (let i = produccion.length - 1; i >= 0; i--) {
             pila.push(produccion[i]);
           }
         }
       } else {
-        return reportarError("No se pudo encontrar una producción válida para", X, a, infoPila);
+        return errorReport("No se pudo encontrar una producción válida para", X, a, stackInfo);
       }
     } else {
-      return reportarError("Token inesperado", X, a, infoPila);
+      return errorReport("Token inesperado", X, a, stackInfo);
     }
   }
 
-  return { esValida: apuntador === tokens.length, infoPila };
+  return { esValida: apuntador === tokens.length, infoPila: stackInfo };
 }
 
-function esNoTerminal(simbolo) {
+function isNotTerminal(simbolo) {
   return /[A-Z]/.test(simbolo);
 }
 
-function manejarPila(accion, X, tokens, apuntador, pila, infoPila) {
+function handleStack(accion, X, tokens, apuntador, pila, infoPila) {
   const mensaje = `${accion}: ${X}, Cadena: ${tokens.slice(apuntador).join(" ")}`;
   infoPila.push(mensaje);
   if (accion === "Pop") pila.pop();
 }
 
-function reportarError(mensaje, X, a, infoPila) {
+function errorReport(mensaje, X, a, infoPila) {
   const reportarError = `Error: ${mensaje} "${X}" con "${a}".`;
   infoPila.push(reportarError);
   return { esValida: false, infoPila, reportarError: reportarError };
 }
 
-function obtenerProduccion(noTerminal, next) {
+function getProduction(noTerminal, next) {
   const producciones = {
     "S": ["I", "A", "B", "V"],
     "B": ["AL", "F"],
@@ -74,8 +74,8 @@ function obtenerProduccion(noTerminal, next) {
 
 export {
   validateVariableDeclaration,
-  esNoTerminal,
-  manejarPila,
-  reportarError,
-  obtenerProduccion
+  isNotTerminal as esNoTerminal,
+  handleStack as manejarPila,
+  errorReport as reportarError,
+  getProduction as obtenerProduccion
 };
